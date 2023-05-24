8bfe278cf3b422cd873be7cbf02f0c46e425893b
leesangjin <tkdwls0628@gmail.com>
2023-05-24 17:48:34 +0900

    [Refactor]: 형태소 추출 한글만 적용되도록 수정

--- extraction/extraction.py
+++ extraction/extraction.py
@@ -1,5 +1,5 @@
 from kiwipiepy import Kiwi
-import os
+import os, re
 
 kiwi = Kiwi()
 
@@ -8,16 +8,18 @@ for file in os.listdir("../crawling/location_keyword"):
     file_list = file.split("_")
     city_name = file_list[0].strip()
 
-    ff = open(city_name + "_extraction.txt", 'w', encoding='UTF-8')
+    if city_name == ".DS":
+        continue
 
+    ff = open("../extraction/extraction_keyword/" + city_name + "_extraction.txt", 'w', encoding='UTF-8')
     with open(os.path.join("../crawling/location_keyword", file), "r") as f:
         lines = f.readlines()
         lines = list(map(lambda s: s.strip(), lines))
         for line in lines:
-            results = []
-            result = kiwi.analyze(line)
+            kor_str = re.sub(r"[^ㄱ-ㅣ가-힣\s]", "", line) # 한글만 추출
+            result = kiwi.analyze(kor_str)
             for token, pos, _, _ in result[0][0]:
-                if len(token) != 1 and pos.startswith('N') or pos.startswith('SL'):
+                if len(token) != 1 and pos.startswith('N'):
                     ff.write(token + "\n")
     ff.close()
 
